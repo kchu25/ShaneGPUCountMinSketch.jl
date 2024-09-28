274880a8cb33ab9e9bce056a051a5a08326838d7
kchu25 <skchu@wustl.edu>
2024-09-28 13:06:42 -0400

    add batch size

--- src/const.jl
+++ src/const.jl
@@ -9,3 +9,8 @@ const default_min_count = 25
 const default_num_threads1D = (128,)
 const default_num_threads2D = (24,24)
 const default_num_threads3D = (8,8,8)
+
+# the batch size for how many data points to process at once
+# i.e. how many sequences' sparse code we should take at once 
+# to write to the sketch
+const batch_size = 5000 
